cd6dd42a55e7633192e61f5d5df5fe47abb3e8d4
silviafds <silviafernandes10@outlook.com.br>
2022-02-26 18:05:40 -0300

    ajeitar config botao

--- Formularios/formulario-1/arquivoJS.js
+++ Formularios/formulario-1/arquivoJS.js
@@ -6,6 +6,7 @@ function validaNomeCompleto() {
         document.getElementById("nomeUsuario").style.borderColor = "red";
         return false;
     }
+    var valorNomeCompleto = 1;
     document.getElementById("nomeUsuario").style.borderColor = "green";
     document.getElementById("erroNome").innerHTML = ""
     return true;
@@ -59,7 +60,7 @@ function validaCPF() {
         document.getElementById("erroCPF").innerHTML = "Informe um CPF válido"
         return false
     }
-
+    var valorCPF = 1;
     document.getElementById("CPF").style.borderColor = "green";
     document.getElementById("erroCPF").innerHTML = ""
     return true
@@ -74,6 +75,7 @@ function validaDataNasc() {
         document.getElementById("erroDtNascimento").innerHTML = "Informe a Data de Nascimento"
         return false;
     } 
+    var valorDataNascimento = 1;
     document.getElementById("dataNascimento").style.borderColor = "green";
     document.getElementById("erroDtNascimento").innerHTML = ""
     return true;
@@ -87,6 +89,7 @@ function validaRegiaoNascimento() {
         document.getElementById("erroRegiaoNasc").innerHTML = "Selecione a Região"
         return false;
     }
+    var valorRegiaoNascimento = 1;
     document.getElementById("regiaoNascimento").style.borderColor = "green";
     document.getElementById("erroRegiaoNasc").innerHTML = ""
     return true;
@@ -100,8 +103,36 @@ function validaTelefone() {
         document.getElementById("erroTelefone").innerHTML = "Digite o número de telefone";
         return false;
     }
-
+    var valorTelefone = 1;
     document.getElementById("telefone").style.borderColor = "green";
     document.getElementById("erroTelefone").innerHTML = "";
     return true;
+}
+
+function validaEmail() {
+    var recebeEmail = document.getElementById("emailUsuario").value;
+
+    if(recebeEmail == "" || recebeEmail.indexOf('@') == -1 || recebeEmail.indexOf('.') == -1) {
+        document.getElementById("emailUsuario").style.borderColor = "red";
+        document.getElementById("erroEmail").innerHTML = "Digite um email válido";
+        return false;
+    }
+    var valorEmail = 1;
+    document.getElementById("emailUsuario").style.borderColor = "green";
+    document.getElementById("erroEmail").innerHTML = "";
+    return true;
+}
+
+function validaDados() {
+    var dadosPreenchidos = valorNomeCompleto + valorCPF + valorTelefone + valorEmail;
+
+    if(dadosPreenchidos == 4) {
+        /*alert("Dados preenchidos com sucesso.");*/
+        document.getElementById("confirmaDados").innerHTML = "Dados preenchidos com sucesso.";
+        return true;
+    }
+    /*alert("Existem dados que não foram preenchidos com suceso. Por favor, preencha para finalizar cadastro.");*/
+    document.getElementById("confirmaDados").innerHTML = "Existem dados que não foram preenchidos com suceso. Por favor, preencha para finalizar cadastro.";
+    return false;
+    
 }
