3cd6a5425735da29e14be126a831be054b4e13f0
silviafds <silviafernandes10@outlook.com.br>
2022-03-04 18:05:44 -0300

    novas funçoes de verificaçao de erro e calculo de imc

--- Projetos Independentes/Projeto 00 - Calculadora IMC/arquivoJs.js
+++ Projetos Independentes/Projeto 00 - Calculadora IMC/arquivoJs.js
@@ -1,23 +1,51 @@
-const { REFUSED } = require("dns");
+function verificaAltura() {
+    var recebeAltura = document.getElementById("altura").value;
 
-function calculaIMC() {
+    if(recebeAltura == "" || recebeAltura < 0) {
+        document.getElementById("erroAltura").innerHTML = "Informe a altura."
+        return 0;
+    }
+    return 1;
+}
+
+function verificaPeso() {
+    var recebePeso = document.getElementById("peso").value;
+
+    if(recebePeso == "" || recebePeso < 0) {
+        document.getElementById("erroPeso").innerHTML = "Informe o peso."
+        return 0;
+    }
+    return 1;
+}
+
+function IMC() {
     var pesoRecebido = document.getElementById("peso").value;
     var alturaRecebida = document.getElementById("altura").value;
 
+    var recebeValidacaoCampoAltura = verificaAltura();
+    var recebeValidacaoCampoPeso = verificaPeso();
+
+    if(recebeValidacaoCampoAltura == 1 && recebeValidacaoCampoPeso == 1) {
+        calculaIMC(pesoRecebido, alturaRecebida);
+    } else {
+        document.getElementById("resultadoRuim").innerHTML = "Preencha todos os campos acima."
+    }
+    
+}
+
+function calculaIMC(pesoRecebido, alturaRecebida) {
     var IMC = pesoRecebido / (alturaRecebida * alturaRecebida);
     var Resultado = parseInt(IMC.toFixed(2));
-    var x = (typeof Resultado);
-    if(Resultado > 40) {
-        alert("Seu IMC é " + Resultado + " você esta com obesidade grave e seu grau de obesidade é III");
-    } else if(Resultado <= 39.90 && Resultado >= 30.0) {
-        alert("Seu IMC é " + Resultado + " você esta com obesidade e seu grau de obesidade é II");
-    } else if(Resultado <= 29.90 && Resultado >= 25.0) {
-        alert("Seu IMC é " + Resultado + " você esta com sobrepeso e seu grau de obesidade é I");
-    } else if(Resultado <= 24.90 && Resultado >= 18.50) {
-        alert("Seu IMC é " + Resultado + " parabéns, continue assim. Seu IMC esta normal.");
-    } else {
-        alert("Seu IMC é " + Resultado + " você esta com magreza, cuidado e seu grau de obesidade é 0");
-    }
 
-    /*document.getElementById("resultado").innerHTML = IMC;*/
+    if(Resultado > 40) {
+        document.getElementById("resultadoRuim").innerHTML = "Seu IMC é " + Resultado + ". Você esta com obesidade grave e seu grau de obesidade é III.";
+    } else if(Resultado <= 39.90 && Resultado >= 30.0) {
+        document.getElementById("resultadoRuim").innerHTML = "Seu IMC é " + Resultado + ". Você esta com obesidade e seu grau de obesidade é II.";
+    } else if(Resultado <= 29.90 && Resultado >= 25.0) {
+        document.getElementById("resultadoRuim").innerHTML = "Seu IMC é " + Resultado + ". Você esta com sobrepeso e seu grau de obesidade é I.";
+    } else if(Resultado <= 24.90 && Resultado >= 18.50) {
+        document.getElementById("resultadoBom").innerHTML = "Seu IMC é " + Resultado + " parabéns, continue assim. Seu IMC esta normal.";
+    } else {
+        document.getElementById("resultadoRuim").innerHTML = "Seu IMC é " + Resultado + ". Você esta com magreza, cuidado e seu grau de obesidade é 0.";
+    }
 }
