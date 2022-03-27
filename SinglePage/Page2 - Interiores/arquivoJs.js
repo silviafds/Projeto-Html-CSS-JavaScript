837d2f890e16fea71c7e1584d471cf09f8d32448
silviafds <silviafernandes10@outlook.com.br>
2022-03-27 14:10:54 -0300

    função de pegar tamanho da tela adicionada

--- SinglePage/Page2 - Interiores/arquivoJs.js
+++ SinglePage/Page2 - Interiores/arquivoJs.js
@@ -1,5 +1,6 @@
 function slide1() {
     document.getElementById("id").src="./images/carrosselImages/minimalismo-1-640px-360px.jpg";
+    tamanhoTela();
     setTimeout("slide2()", 2000)
 }
 
@@ -18,12 +19,26 @@ function slide4() {
     setTimeout("slide1()", 2000)
 }
 
+var windowWidth;
+var windowHeight;
+function tamanhoTela() {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    /*document.querySelector("#tela").innerHTML = window.innerWidth;*/
+}
+
 function bigImg(x) {
     x.style.height = "353px";
     x.style.width = "520px";
+
+    if(windowWidth <= 625) {
+        x.style.height = "";
+        x.style.width = "240px";
+    }
 }
 
 function normalImg(x) {
     x.style.height = "";
     x.style.width = "";
 }
+
